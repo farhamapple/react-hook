c0563c350183b7f255e8407c31e322db9f03207b
farham-dell <farh001@brin.go.id>
2023-04-25 09:34:28 +0700

    First

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../css/General.module.css";
+
+function LandingPage() {
+  return (
+    <section>
+      <main className="inner cover">
+        <h1 className="cover-heading">Cover your page.</h1>
+        <p className="lead">
+          Cover is a one-page template for building simple and beautiful home
+          pages. Download, edit the text, and add your own fullscreen background
+          photo to make it your own.
+        </p>
+        <p className="lead"></p>
+      </main>
+    </section>
+  );
+}
+
+export default LandingPage;
---
+++ src/pages/UseStatePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function UseStatePage(){
+
+    return <></>
+
+}
+
+export default UseStatePage
